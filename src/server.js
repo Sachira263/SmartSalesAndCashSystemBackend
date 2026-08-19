932639214f0a2023fa013ac6d96397f363bcfdce
unknown <sachirasanthush81@gmail.com>
2026-08-19 14:01:52 +0530

    Add automated root user seeding and bcryptjs

--- src/server.js
+++ src/server.js
@@ -1,18 +1,49 @@
 require('dotenv').config();
 const express=require("express");
-
+const bcrypt = require("bcryptjs");// get new package for password hashing
 const db = require('./config/db');
 
 
 const app=express();
 const PORT=5001;
 
+// process of create root user automatically when server starts
+async function createRootUser() {
+    try {
+        // 1. Check if a Root User already exists in the database
+        const checkUser = await db.query("SELECT * FROM users WHERE role = 'Root'");
+
+        // 2. If no Root User exists, create one
+        if (checkUser.rows.length === 0) {
+            // Hash the temporary password (Admin@123)
+
+            const hashedPassword = await bcrypt.hash('Admin@123', 10); 
+
+            const insertQuery = `
+                INSERT INTO users (name, email, password_hash, role, is_active)
+                VALUES ($1, $2, $3, $4, $5)
+            `;
+            const values = ['Super Admin', 'admin@smartsales.com', hashedPassword, 'Root', true];
+
+            await db.query(insertQuery, values);
+            console.log('Root User created successfully! ');
+        } else {
+            console.log('Root User already exists. ');
+        }
+    } catch (err) {
+        console.error('Error creating root user ', err.message);
+    }
+}
+
 
 // when server starts, test connect with database
 db.query('SELECT NOW()')
-  .then(() => console.log('Database Connected Successfully! ✅'))
+  .then(() => {
+     console.log('Database Connected Successfully! ✅');
+      createRootUser(); // Call the function to create the root user
+  })
   .catch(err => console.error('Database Connection Error ❌', err.stack));
-  
+
 
 app.get('/api/test', async (req, res) => {
   try {
