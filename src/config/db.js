61e8bc48af0862df81a60fc7beeb9aa77c0f1e0b
unknown <sachirasanthush81@gmail.com>
2026-08-19 12:23:59 +0530

    Add PostgreSQL connection and setup gitignore

---
+++ src/config/db.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+// Create a pool instance using env variables
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+});
+
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+};
+
+
+
+
+
--- src/server.js
+++ src/server.js
@@ -1,7 +1,32 @@
+require('dotenv').config();
 const express=require("express");
+
+const db = require('./config/db');
+
 
 const app=express();
 const PORT=5001;
+
+
+// when server starts, test connect with database
+db.query('SELECT NOW()')
+  .then(() => console.log('Database Connected Successfully! ✅'))
+  .catch(err => console.error('Database Connection Error ❌', err.stack));
+  
+
+app.get('/api/test', async (req, res) => {
+  try {
+    const result = await db.query('SELECT NOW()');
+    res.json({ message: "Connected successfully!", time: result.rows[0] });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Database connection error");
+  }
+});
+
+
+
+
 
 const server=app.listen(PORT, ()=>{
 
